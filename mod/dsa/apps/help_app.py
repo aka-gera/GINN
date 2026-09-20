418707431224311569d3f92834bda612d9ca6445
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2026-09-19 20:01:46 -0400

    Add files via upload

--- mod/dsa/apps/help_app.py
+++ mod/dsa/apps/help_app.py
@@ -86,11 +86,16 @@ class DSAPage(dropdown_callback,algorithm,model_pred):
         self.obj_org_path=dict_param.get('obj_org_path',None)
         self.file_path_org,self.file_path_data=file_path_org,file_path_data
 
-        path_diroi=os.path.join(file_path_org, 'data') 
+        path_diroi=os.path.join(file_path_org, 'data')  
+        spath=os.path.join(path_diroi, 'neld_names_all.txt')
+        if not os.path.exists(spath):
+            ttname=os.path.basename(file_path_org)
+            ggname=os.path.basename(os.path.dirname(file_path_org))
+            path_diroi=os.path.join(file_path_data, 'files',ggname,ttname,'data') 
+            spath=os.path.join(path_diroi, 'neld_names_all.txt')
+        self.neld_names_all=neld_names_all=np.loadtxt(spath,dtype=str,ndmin=1)
         true_keys=np.loadtxt(os.path.join(path_diroi, 'true_keys.txt'),dtype=str,ndmin=1)
-        self.neld_names_all=neld_names_all=np.loadtxt(os.path.join(path_diroi, 'neld_names_all.txt'),dtype=str,ndmin=1)
 
- 
 
         # if nam_gen_show is None: 
         #     file_path_org=file_path_org or os.path.join(os.getcwd(),'files') 
