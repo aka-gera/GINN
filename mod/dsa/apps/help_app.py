2c9de7d94d5329c82983e0d53613fae432eec2f9
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2026-09-19 22:53:29 -0400

    Fix indentation and remove unnecessary line

--- mod/dsa/apps/help_app.py
+++ mod/dsa/apps/help_app.py
@@ -174,8 +174,7 @@ class DSAPage(dropdown_callback,algorithm,model_pred):
 
         configs=dict_param['configs']
 
-        path_dir=os.path.join(self.file_path_org, 'data','pinn_dir_data_all.txt') 
-        pinn_dir_data_all=dmode.pinn_dir_data_all
+        path_dir=os.path.join(self.file_path_org, 'data','pinn_dir_data_all.txt')  
         if os.path.exists(path_dir):
             pinn_dir_data_all=[mm for mm in list(np.loadtxt(path_dir,dtype=str)) ]
 
